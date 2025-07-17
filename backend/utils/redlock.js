2dfcd025079ac765e8f942a0fd05e94c60f5e652
KyawNyanMyat <kyawnyanmyat40@gmail.com>
2025-07-17 14:44:04 +0630

    Deployment

--- backend/utils/redlock.js
+++ backend/utils/redlock.js
@@ -1,9 +1,7 @@
 import Redis from "ioredis";
 import Redlock from "redlock";
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.REDIS_PORT || 6379,
+const redis = new Redis(process.env.REDIS_URL,{
     lazyConnect: true, // only connect when you call redis.connect()
     enableOfflineQueue: false, //Throws key:value immediately if disconnected
     connectTimeout: 5000,
